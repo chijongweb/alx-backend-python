28c8dd23adc28ddce673dcc4144af21635438539
chijongweb <160375788+chijongweb@users.noreply.github.com>
2025-05-18 11:05:40 +0200

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -7,7 +7,7 @@ def stream_users():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="T!g3rfish",
+            password="********",
             database="ALX_prodev"
         )
         cursor = connection.cursor(dictionary=True)
