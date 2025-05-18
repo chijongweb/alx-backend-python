af66aee0350479c3b08816bdf9333d831cecfa5e
chijongweb <160375788+chijongweb@users.noreply.github.com>
2025-05-18 11:01:16 +0200

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,29 @@
+import mysql.connector
+from mysql.connector import Error
 
+def stream_users():
+    
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="T!g3rfish",
+            database="ALX_prodev"
+        )
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_data")
+
+        for row in cursor:  
+            yield row
+
+    except Error as e:
+        print(f"Error fetching data: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+# Example usage:
+if __name__ == "__main__":
+    for user in stream_users():
+        print(user)
