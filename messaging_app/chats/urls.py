0e27f4a6faa092024785a3c1879c9f6551baf367
chijongweb <bchijongwe1@gmail.com>
2025-06-01 13:06:26 +0200

    Add

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -6,6 +6,15 @@ router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
+convo_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(convo_router.urls)),
 ]
+
+
+
+
+
--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls')), 
+
+   
+
 ]
