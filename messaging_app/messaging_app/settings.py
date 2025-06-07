0e969ef5472f7354c9d2ede424d1eefc70de8750
chijongweb <bchijongwe1@gmail.com>
2025-06-07 15:59:19 +0200

    2nd commit

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -137,12 +137,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',  # <-- Add this
-    'PAGE_SIZE': 20,  # Optional fallback
+     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',  
+    'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ],
-
 }
