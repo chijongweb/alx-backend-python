7d343911217101c8627e0244d9ebf402065ebf10
chijongweb <bchijongwe1@gmail.com>
2025-06-08 01:36:51 +0200

    commit

--- Django-Middleware-0x03/manage.py
+++ Django-Middleware-0x03/manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Middleware_Project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
