ce0551d165c6180c63498837f3104af3e122b5ff
chijongweb <bchijongwe1@gmail.com>
2025-06-15 13:10:08 +0200

    initial +2

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
+
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -10,3 +11,21 @@ def create_notification_on_message(sender, instance, created, **kwargs):
             message=instance,
             content=f"You have a new message from {instance.sender.username}."
         )
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edits(sender, instance, **kwargs):
+    # Only check for updates to existing messages
+    if instance.id:
+        try:
+            old_instance = Message.objects.get(id=instance.id)
+            if old_instance.content != instance.content:
+                # Log the old content to history
+                MessageHistory.objects.create(
+                    message=old_instance,
+                    old_content=old_instance.content
+                )
+                # Mark message as edited
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass 
