e8f26eda4dc9aaef879bb66cb500517f8716bc7c
chijongweb <bchijongwe1@gmail.com>
2025-06-15 14:39:17 +0200

    initial +2

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class UnreadMessagesManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(receiver=user, read=False).only('id', 'sender', 'content', 'timestamp')
+
+
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
@@ -10,6 +15,10 @@ class Message(models.Model):
     edited = models.BooleanField(default=False)
     edited_by = models.ForeignKey(User, null=True, blank=True, related_name='edited_messages', on_delete=models.SET_NULL)
     parent_message = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)  # New field to indicate if message has been read
+
+    objects = models.Manager()  # Default manager
+    unread_messages = UnreadMessagesManager()  # Custom manager for unread messages
 
     def __str__(self):
         reply_note = " (Reply)" if self.parent_message else ""
